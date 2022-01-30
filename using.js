768a9e777221e7e848f0d9056370b15bdce52b46
masudrana <masudranaacb@gmail.com>
2022-01-30 21:52:53 +0600

    for loop decriment

--- using.js
+++ using.js
@@ -1,11 +1,4 @@
-
-// function addition 1-5 number
-function addNumber(number){
-    let addition=0;
-    for(let i=1; i<=number; i++){
-        addition=addition+i;
-    }
-    return addition;
+// decriment the code
+for(var i=20; i>=1; i--){
+    console.log(i);
 }
-const result=addNumber(5);
-console.log(result);
